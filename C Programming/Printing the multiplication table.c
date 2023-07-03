76a6bce3348385012e61496abc22c4b115ca4a72
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-07-03 21:59:11 +0530

    adding new code in c program

---
+++ C Programming/Printing the multiplication table.c
@@ -0,0 +1,9 @@
+ Print the multiplication table
+  int n;
+  int i = 0;
+  printf("Enter the number to be printed : \n");
+  scanf("%d",&n);
+  for ( i = 1; i < 11; i++)
+  {
+     printf(" %d X %d = %d\n",n,i ,n*i);
+  }
