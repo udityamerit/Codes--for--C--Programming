fc07c072a1d68ac963ef7a7d58d1e980ebf6d896
Uditya Narayan Tiwari <uditmerit@gmail.com>
2023-08-06 08:01:52 +0530

    add new codes in c

--- C Programming/Exercise_5_Array_Reversal_36.c
+++ C Programming/Exercise_5_Array_Reversal_36.c
@@ -1,11 +1,44 @@
+// Addition of Two 1-D Arrays
 #include<stdio.h>
-
-void arrayRev(arg1)
-{
-
-}
 int main(){
-         int arr[] = {1,2,3,4,5,6,67};
+        int arr[5];
+        int i = 0;
+        printf("Enter your first array: \n");
+        for ( i = 0; i < 5; i++)
+        {
+                printf("Enter the elements of the array: ");
+                scanf("%d",&arr[i]);
+        }
+        printf("Your first array is = ");
+        for ( i = 0; i < 5; i++)
+        {
+                printf("%d ",arr[i]);
+        }
+        printf("\n\n");
 
-         return 0;
+        // I am Going to copy the first array 
+        // and modify is in second array
+
+        int arr1[5];
+        printf("Enter your second array: \n");
+        for ( i = 0; i < 5; i++)
+        {
+                printf("Enter the elements of the array: ");
+                scanf("%d",&arr1[i]);
+        }
+        printf("Your second array is = ");
+        for ( i = 0; i < 5; i++)
+        {
+                printf("%d ",arr1[i]);
+        }
+        printf("\n\n");
+
+        printf("Sum of two arrays is : ");
+        for ( i = 0; i < 5; i++)
+        {
+                printf("%d ",arr[i]+arr1[i]);
+        }
+        return 0;
+        
+        
 }
--- Practice set of C Programming/Practice_set_7_array.c
+++ Practice set of C Programming/Practice_set_7_array.c
@@ -17,3 +17,103 @@ int main(){
       
       return 0 ;
 }
+
+// printing two 1-d array using functions
+
+#include<stdio.h>
+
+int func1 (){
+        int arr[5];
+        for (int i = 0; i < 5; i++)
+        {
+                printf("Enter the element of an array: ");
+                scanf("%d",&arr[i]);
+        }
+         for (int i = 0; i < 5; i++)
+        {
+            
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        return arr[5];
+}
+
+int func2 (){
+        int arr1[5];
+        for (int i = 0; i < 5; i++)
+        {
+                printf("Enter the element of an array: ");
+                scanf("%d",&arr1[i]);
+        }
+         for (int i = 0; i < 5; i++)
+        {
+            
+            printf("%d ", arr1[i]);
+            
+        }
+         printf("\n");
+        return arr1[5];
+
+}
+
+int main(){
+
+        printf("Enter the first array:\n");
+        func1();
+        printf(' ');
+        printf("Enter the second array:\n");
+        func2();
+        printf(' ');
+        
+        return 0;
+}
+
+// printing sum of two 1-d arrays
+
+#include<stdio.h>
+
+int main (){
+        int arr[5];
+         int sum = 0;
+        printf("Enter the first array:\n");
+        for (int i = 0; i < 5; i++)
+        {
+                printf("Enter the element of an array: ");
+                scanf("%d",&arr[i]);
+        }
+        printf("Your first array is: ");
+         for (int i = 0; i < 5; i++)
+        {
+            
+            printf("%d ", arr[i]);
+        }
+        printf("\n\n");
+       
+        printf("Enter the second array:\n");
+        int arr1[5];
+        for (int i = 0; i < 5; i++)
+        {
+                printf("Enter the element of an array: ");
+                scanf("%d",&arr1[i]);
+        }
+        
+        printf("Your second array is: ");
+         for (int i = 0; i < 5; i++)
+        {
+            
+            printf("%d ", arr1[i]);
+            
+        }
+
+         printf("\n\n");
+        printf("You new array whose sum of two array is :");
+
+        for (int i = 0; i < 5; i++)
+        {
+                printf("%d ",arr[i]+arr1[i]);
+        }
+
+        return 0;
+}
+
+
