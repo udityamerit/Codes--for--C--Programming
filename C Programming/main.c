48b276783d9b9e20521d1362e9a29f40b14c0aea
Uditya Narayan Tiwari <uditmerit@gmail.com>
2024-08-11 20:52:15 +0530

    adding the c code

--- C Programming/main.c
+++ C Programming/main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 int main()
-{ printf("Hello World");
-return 0 ;
+{ 
+        int number1 =34;
+        double number2 = 78.32;
+        printf("%d %lf",number1,number2);
+        return 0 ;
+
+        // for data type in c/c++ 
+        /* int = %d
+        long = %ld
+        float = %f
+        doubble = %lf
+        char = %c
+          
+        */
 }
